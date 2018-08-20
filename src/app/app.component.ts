662ee1f50223b8295d681a4dbd676973b85f9ec5
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-08-20 17:27:02 +0900

    新規追加

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h2>TwoWay Binding Sample</h2>
+  `
+})
+export class AppComponent {
+  num: number = 1;
+}
